bba3a43abfbc0a4b13bb8bd819317b6b0405b651
Pranav Mohril <pranavmohril@gmail.com>
2024-02-04 09:27:22 -0500

    firestore push added

--- server.py
+++ server.py
@@ -40,16 +40,22 @@ async def extract_text(item:Item):
     storage.download(item.imageName,file)
     # text = await ocr.read_image(file)
     data = {}
-    data['patient'] = 'John Doe'
-    data['doctor'] = 'Greg House'
-    data['medicine'] = [{'name':'crocine', 'dose':'Once a week','totaldose':'5'},{'name':'Vicodin', 'dose':'3 per day','totaldose':'10000'}]
+    data['patient'] = 'Jane Doe'
+    data['doctor'] = 'James Wilson'
+    data['medicine'] = [{'name':'D2o', 'dose':'1 per week','totaldose':'4'},{'name':'B-blocker', 'dose':'1 per day','totaldose':'7'}]
+    data['pharmacyID'] = '1234'
+    # After we get results, add to database
+    db = firebase.database()
+    db.child("prescription").push(data)
+    print("Data pushed to firebase")
+
     return data
 
 
-@app.post("/api/v1/extract_text")
-async def extract_text(image: UploadFile = File(...)):
-    print('Image data on server',image)
-    # temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
-    # text = await ocr.read_image(temp_file)
+# @app.post("/api/v1/extract_text")
+# async def extract_text(image: UploadFile = File(...)):
+#     print('Image data on server',image)
+#     # temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
+#     # text = await ocr.read_image(temp_file)
     
-    return {"filename": image.filename, "text": 'Sample OCR from the server'}
+#     return {"filename": image.filename, "text": 'Sample OCR from the server'}
